4d783696ac03c59d137da08b56c7043964deffe5
Silva <sifsilva@devroomsa.com>
2022-02-28 00:34:09 +0200

    Adding Connection Resiliency for connection failer

--- InventorySystem/Program.cs
+++ InventorySystem/Program.cs
@@ -1,23 +1,38 @@
 using Infrastructure.Data;
+using InventorySystem;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add framework services.
 builder.Services.AddDbContext<InventoryDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("InventorySystemConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("InventorySystemConnection"),
+         sqlServerOptionsAction: sqlOptions =>
+         {
+             //Adding Connection Resiliency for connection failer
+             sqlOptions.EnableRetryOnFailure(maxRetryCount: 5,
+             maxRetryDelay: TimeSpan.FromSeconds(30),
+             errorNumbersToAdd: null);
+         });
 });
 
 builder.Services.AddDistributedMemoryCache();
-
+// Adding Session service.
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(3);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
+
+ConfigurationManager configuration = builder.Configuration;
+configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+
+var username = configuration.GetSection("EmailConfiguration").GetSection("Username").Value;
+var pw = configuration.GetSection("EmailConfiguration").GetSection("Password").Value;
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
